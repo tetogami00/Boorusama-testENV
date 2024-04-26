c42333cba0568dedafe0216936683b76747e58ff
khoa.nguyen <nguyenduckhoa1996@gmail.com>
2024-04-26 17:56:43 +0700

    feat: delete Firebase

--- lib/foundation/tracking.dart
+++ lib/foundation/tracking.dart
@@ -2,10 +2,6 @@
 import 'package:boorusama/core/feats/settings/settings.dart';
 import 'package:boorusama/foundation/analytics.dart';
 import 'package:boorusama/foundation/error.dart';
-import 'firebase/firebase.dart';
-
-export 'firebase/firebase.dart';
 
 Future<(AnalyticsInterface? analytics, ErrorReporter? reporter)>
-    initializeTracking(Settings settings) =>
-        ensureFirebaseInitialized(settings);
+    initializeTracking(Settings settings) async => (null, null);
